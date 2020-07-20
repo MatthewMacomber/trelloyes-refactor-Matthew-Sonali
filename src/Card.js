3c8afd28b38f58467903df2166d09e5904815e82
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-20 12:25:44 -0700

    add Card component

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div id={this.props.id} class="Card">
+                <button type="button">delete</button>
+                <h3>{this.props.title}</h3>
+                <p>{this.props.content}</p>
+            </div>
+        );
+    }
+}
+
+
+
+
+
